4d1578a6f835071164f1204ed7a858002fd49ad3
ashot <ashot.arakelyan@picsart.com>
2016-09-25 16:54:11 +0400

    last remove test commit

--- app/src/main/java/com/example/ashot/gradleparams/MainActivity.java
+++ app/src/main/java/com/example/ashot/gradleparams/MainActivity.java
@@ -30,8 +30,6 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    // again add test comment
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
